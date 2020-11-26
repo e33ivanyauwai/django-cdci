d5b975588b2c4a390b87306e78b258c00a022748
Ivan Yau <kwyau@Ivan-MacBookAir.local>
2020-11-26 16:44:54 +0800

    First commit.

--- polls/views.py
+++ polls/views.py
@@ -16,4 +16,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.  This is version 2.0, patched.")
